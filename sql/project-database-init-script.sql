f6dc503e847c8cee28a455b88418bbc1c689ceac
justinehobbs <jhob387@aucklanduni.ac.nz>
2023-10-25 21:01:09 +1300

    justine-AccountSQL

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -5,12 +5,29 @@
  */
 
 drop table if exists test;
+drop table if exists Avatar;
+drop table if exists Account;
 
-create table test (
-    id integer not null primary key,
-    stuff text  
+-- create table test (
+--     id integer not null primary key,
+--     stuff text  
+-- );
+
+-- insert into test (stuff) values
+--     ('Things'),
+--     ('More things');
+
+create table Avatar (
+    AvatarID int primary key,
+    ImageFilePath VARCHAR(255) -- Store the file path or URL here
 );
 
-insert into test (stuff) values
-    ('Things'),
-    ('More things');
+create table Account (
+    AccountID Iint primary key,
+    Password VARCHAR(255),
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    EmailAddress VARCHAR(100),
+    AvatarID INT UNIQUE,  -- Unique constraint for one-to-one relationship
+    FOREIGN KEY (AvatarID) REFERENCES Avatar(AvatarID)
+);
