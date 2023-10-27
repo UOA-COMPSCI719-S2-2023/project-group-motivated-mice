1ddc572e7c4b76c28b852792a956c79d923b9869
justinehobbs <jhob387@aucklanduni.ac.nz>
2023-10-27 22:37:08 +1300

    justine-updatedSQL

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -4,30 +4,67 @@
  * required.
  */
 
-drop table if exists test;
+--Drop tables if they already exist
 drop table if exists Avatar;
 drop table if exists Account;
+drop table if exists Articles;
+drop table if exists Images;
+drop table if exists Comments;
 
--- create table test (
---     id integer not null primary key,
---     stuff text  
--- );
-
--- insert into test (stuff) values
---     ('Things'),
---     ('More things');
-
+--Create the Avatar table
 create table Avatar (
     AvatarID int primary key,
     ImageFilePath VARCHAR(255) -- Store the file path or URL here
 );
 
+--Create the Account table
 create table Account (
-    AccountID Iint primary key,
-    Password VARCHAR(255),
+    AccountID int primary key,
+    UserName VARCHAR(50),
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
     EmailAddress VARCHAR(100),
+    DateOfBirth date,
+    About text,
+    Likes int,
+    --Unsure if we should store password in seperate table for authentication?
+    HashedPassword CHAR(60) NOT NULL,
     AvatarID INT UNIQUE,  -- Unique constraint for one-to-one relationship
     FOREIGN KEY (AvatarID) REFERENCES Avatar(AvatarID)
 );
+
+
+-- Create the Articles table
+create table Articles (
+    ArticleID int primary key,
+    UserID int,
+    PublishDate timestamp,
+    Likes int,
+    Title VARCHAR(255) NOT NULL,
+    Content text,
+    FOREIGN KEY (UserID) REFERENCES Account(AccountID)  -- Establish the relationship
+);
+
+
+-- Create the Image table
+create table Images (
+    ImageID int primary key,
+    ArticleID int,  -- Associates an image with an article
+    ImageURL VARCHAR(255) NOT NULL,  -- Stores the URL to the image file
+    FOREIGN KEY (ArticleID) REFERENCES Articles(ArticleID)
+);
+
+-- Create the Comments table
+create table Comments (
+    CommentID int primary key,
+    ArticleID int,  -- Associates a comment with an article
+    UserID int,     -- Identifies the user who made the comment
+    CommentDate timestamp,
+    CommentText text,
+    FOREIGN KEY (ArticleID) REFERENCES Articles(ArticleID),
+    FOREIGN KEY (UserID) REFERENCES Account(AccountID)
+);
+
+-- Update the Articles table
+alter table Articles
+ADD Comments INT DEFAULT 0; -- Add a column to track the number of comments on each article
