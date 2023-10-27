c26b5d35503175ebe453d12f8c8931fa8b4aaf2d
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-10-28 11:19:19 +1300

    starting to make article handlbars and router

---
+++ routes/article-routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const testDao = require("../modules/test-dao.js");
+
+router.get("/entry", function (req, res) {
+res.locals.title;
+res.locals.publishDate;
+res.locals.likes;
+res.locals.title;
+res.locals.image;
+res.locals.content;
+res.locals.userId;
+res.locals.
+// res.locals.locationId; For map API
+
+});
+
+module.exports = router;
