2e64f191abc807d0502b913ca34971ccd265b6bb
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-10-28 23:42:04 +1300

    quill has some basic features.

--- public/custom-quill.js
+++ public/custom-quill.js
@@ -1,8 +1,15 @@
 window.addEventListener("load", function () {
     console.log("the page is loaded;")
-    var quill = new Quill('#editor', {
+    var container = document.querySelector("#editor");
+    var container = document.querySelector("#toolbar");
+
+    var toolbarOptions = [
+        { size: [ 'small', false, 'large', 'huge' ]}
+      ];
+    var options = {
+        placeholder: 'Compose an epic...',
+       
         theme: 'snow'
-    });
+    };
+    var editor = new Quill(container, options);
 });
-
-
