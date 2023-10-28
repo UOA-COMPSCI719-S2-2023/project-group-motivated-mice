a79aea2337f9a6827db66c5ad89b6882b87b4d33
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-10-29 00:24:58 +1300

    toolbar of Quill has image upload button

--- public/custom-quill.js
+++ public/custom-quill.js
@@ -4,11 +4,26 @@ window.addEventListener("load", function () {
     var container = document.querySelector("#toolbar");
 
     var toolbarOptions = [
-        { size: [ 'small', false, 'large', 'huge' ]}
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote'],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent      
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+        ['image'],
+
+        ['clean']                                         // remove formatting button
       ];
+      
     var options = {
         placeholder: 'Compose an epic...',
-       
+        formats: 'Image', 
+        modules: {
+            toolbar: toolbarOptions
+          },
         theme: 'snow'
     };
     var editor = new Quill(container, options);
