791a6213ca6484e67fd212077cd40f9fdf01357b
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-10-29 17:22:28 +1300

    Event listener to submit a form

--- public/custom-quill.js
+++ public/custom-quill.js
@@ -1,30 +1,41 @@
 window.addEventListener("load", function () {
-    console.log("the page is loaded;")
-    var container = document.querySelector("#editor");
-    var container = document.querySelector("#toolbar");
+  console.log("the page is loaded;")
+  const container = document.querySelector("#editor");
 
-    var toolbarOptions = [
-        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-        ['blockquote'],
-        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent      
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-        [{ 'font': [] }],
-        [{ 'align': [] }],
-        ['image'],
+  const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote'],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent      
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+    ['image'],
 
-        ['clean']                                         // remove formatting button
-      ];
-      
-    var options = {
-        placeholder: 'Compose an epic...',
-        formats: 'Image', 
-        modules: {
-            toolbar: toolbarOptions
-          },
-        theme: 'snow'
-    };
-    var editor = new Quill(container, options);
+    ['clean']                                         // remove formatting button
+  ];
+
+  const options = {
+    placeholder: 'Compose an epic...',
+    formats: 'Image',
+    modules: {
+      toolbar: toolbarOptions
+    },
+    theme: 'snow'
+  };
+  const editor = new Quill(container, options);
+
+
+
+  //in MDN submit event
+  function logForm(event) {
+    console.log("form submitted");
+  }
+
+
+  
+  const form = document.querySelector("form");
+  form.addEventListener("submit", logForm);
 });
