945c73e300f274b2e5c3f7767b961d38df201d14
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-10-31 11:27:34 +1300

    function to enter data of post to database

--- modules/article-author.js
+++ modules/article-author.js
@@ -2,6 +2,11 @@ const SQL = require("sql-template-strings");
 const dbPromise = require("./database.js");
 
 
-async function retrieveCurrentUser(){
-    
+async function createPost(AuthorId, content, title) {
+    const db = await dbPromise;
+
+    await db.run(SQL`
+        INSERT INTO articles (UserID, Content, Title, Likes, PublishDate)
+        VALUES(${AuthorId}, ${content}, ${title}, 0, datetime('now'))`);
 }
+
