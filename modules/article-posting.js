996e2db59482f1d08ed5f8b9afdaf90ca9efe083
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-11-01 10:12:39 +1300

    adding function to get article ID

--- modules/article-posting.js
+++ modules/article-posting.js
@@ -9,7 +9,13 @@ async function createPost(AuthorId, content, title) {
         INSERT INTO articles (UserID, Content, Title, Likes, PublishDate)
         VALUES(${AuthorId}, ${content}, ${title}, 0, datetime('now'))`);
 }
+async function retrieveSingleArticleId(AuthorId, content) {
+    const db = await dbPromise;
+    const articleId = await db.get(SQL`SELECT ArticleID FROM Articles WHERE UserID = ${AuthorId} AND Content = ${content}`) 
+    return await articleId;
+};
 
 module.exports = {
-    createPost
+    createPost,
+    retrieveSingleArticleId
 };
