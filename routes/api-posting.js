b8697c8156062f5d4c341697758c1eeb9371b6df
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-11-01 11:08:59 +1300

    variables to render preview page

--- routes/api-posting.js
+++ routes/api-posting.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-let bodyParser = require('body-parser');
 
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json({}));
@@ -19,5 +18,14 @@ router.post("/writeArticle", async function (req,res){
  const userId = 1;
  const content = req.body.article;
  await postArticle.createPost(userId, content, title);
+ const previewArticleId = await postArticle.retrieveSingleArticleId(userId, content);
+ console.log("🚀 ~ file: api-posting.js:22 ~ previewArticleId:", previewArticleId)
+ res.locals.title = title;
+ res.locals.userName;
+ res.locals.articleEntry = true;
+ res.locals.preview = true;
+ res.locals.content = content;
+ res.locals.about;
+ res.render("article");
 })
 module.exports = router;
