1fee862054b81b28ddef1b1317af2b25879087b7
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-10-31 17:48:28 +1300

    addtional code for big images upload

--- app.js
+++ app.js
@@ -17,8 +17,12 @@ app.engine("handlebars", handlebars.engine({
 app.set("view engine", "handlebars");
 
 // Setup body-parser
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 // Setup cookie-parser
 const cookieParser = require("cookie-parser");
