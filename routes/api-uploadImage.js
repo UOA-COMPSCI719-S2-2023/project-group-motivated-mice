5d44f5f052c4493a8e4bfa58b52ef628b337b07c
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-11-02 20:49:24 +1300

    comment about uploading image router function

--- routes/api-uploadImage.js
+++ routes/api-uploadImage.js
@@ -5,7 +5,7 @@ router.use(express.json({}));
 const upload = require("../middleware/multer-uploader.js");
 const imageUpload = require("../modules/upload-image.js");
 
-
+//Put uploaded images to the Preview folder so that the images could be rendered and user can choose one of them as the thumbnail
 router.post("/uploadImage", upload.single("imageFile"), async function (req, res) {
     const fileInfo = req.file;
     const userID = 1;
