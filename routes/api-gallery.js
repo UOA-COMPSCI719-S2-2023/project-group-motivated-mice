041090f1b9ae73ccd9743ca63b6205f23ab5ad71
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-11-04 10:45:48 +1300

    trying to get thumbnail to articles screen

--- routes/api-gallery.js
+++ routes/api-gallery.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const articles = require("../modules/posting-dao.js");
+const reading = require("../modules/read-article.js")
 
 router.get("/gallery", async function (req, res) {
-    const articlesList = await articles.retrieveAllArticles();
+    let articlesList = await articles.retrieveAllArticles();
+    console.log("🚀 ~ file: api-gallery.js:8 ~ articlesList:", articlesList)
+
+    const thumbnailList = await articles.retrieveAllThumbnails();
+    
     res.locals.articles = articlesList;
+    res.locals.images = thumbnailList;
    
 
     res.render("gallery");
