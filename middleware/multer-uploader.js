1f1bd65153b903b66b631c38201ada3e0297bdcd
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-11-03 23:06:46 +1300

    increase the field size limit of multer

--- middleware/multer-uploader.js
+++ middleware/multer-uploader.js
@@ -1,7 +1,8 @@
 // Setup multer (files will temporarily be saved in the "temp" folder).
 const multer = require("multer");
 const upload = multer({
-    dest: "temp"
+    dest: "temp",
+    limits: { fieldSize: 25 * 1024 * 1024 }
 });
 
 // Export the "upload" object, which we can use to actually accept file uploads.
