6a858e5dee41480369e3df8cf53a8da52acec342
junbo.shim <jshi621@aucklanduni.ac.nz>
2023-11-03 23:08:40 +1300

    processing uploaded files and content

--- routes/api-posting.js
+++ routes/api-posting.js
@@ -5,31 +5,36 @@ router.use(express.urlencoded({ extended: true }));
 router.use(express.json({}));
 
 const fs = require("fs");
+const upload = require("../middleware/multer-uploader.js");
 
 const testDao = require("../modules/test-dao.js");
 const postArticle = require("../modules/make-article.js");
-
+const uploadImage = require("../modules/upload-image.js")
 
 router.get("/posting", function (req, res) {
-    res.locals.articleEntry = true;
-    let fileNames = fs.readdirSync("public/images/preview");
-    res.locals.images = fileNames;
-    res.render("posting");
+
+  res.render("posting");
 });
 
-router.post("/writeArticle", async function (req,res){
- const title = req.body.title;
- const userId = 2;
- const content = req.body.article;
- await postArticle.createPost(userId, content, title);
- res.locals.title = title;
- res.locals.preview = true;
- res.locals.content = content;
- 
- const message = `Content Uploaded!`;
+//register the article in the database.
+router.post("/writeArticle",  upload.array("imageFile"), async function (req, res) {
+  const userId = 2;
+  const images = req.files;
+  uploadImage.linkImageToArticle(images, userId);
+  const title = req.body.title;
+  
+  // await uploadImage.linkImageToArticle(imageData, userId);
+  const content = req.body.article;
+  await postArticle.createPost(userId, content, title);
+  res.locals.title = title;
+  res.locals.articleEntry = true;
+  res.locals.content = content;
 
- res.setToastMessage(message);
+  const message = `Content Uploaded!`;
+
+  res.setToastMessage(message);
   res.render("article");
-})
+});
+
 
 module.exports = router;
