35847e55ae77109a7bfdfbd3e2b652753d8a993f
odettecolebrook <ocol596@aucklanduni.ac.nz>
2023-11-05 08:33:47 +0000

    delete test dao again

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const testDao = require("../modules/test-dao.js");
+
 const accountDAO = require("../modules/account-dao.js"); // needed for homepage additions
 const locationDAO = require("../modules/location-dao.js");
 const articleDAO = require("../modules/article-dao.js");
@@ -14,13 +14,8 @@ const articleDAO = require("../modules/article-dao.js");
 router.get("/",async function(req, res) {
     if(req.cookies.authToken){
     res.locals.loggedIn = "true";
-    res.locals.title = "Verified user!";
-    res.locals.allTestData = await testDao.retrieveAllTestData();
-    }
-    else{
-    res.locals.title = "NOT VERIFIED";
-    res.locals.loggedIn = null;
-    }
+ }
+
 
     res.render("home");
   
